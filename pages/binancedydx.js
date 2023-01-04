15fbee518c3a83832f161aa5fa6654fa2900c6be
2018009070 <youngan@hanyang.ac.kr>
2023-01-04 11:50:21 +0900

    binance dydx analyze page

--- pages/binancedydx.js
+++ pages/binancedydx.js
@@ -15,23 +15,37 @@ export default function Binancedydx() {
   const [binanceMarkPrice, setBinanceMarkPrice] = useState(1)
   const [binanceMarkPriceTime, setBinanceMarkPriceTime] = useState(null)
   const [binanceIndexPrice, setBinanceIndexPrice] = useState(1)
+  const [binanceCurrentPrice, setBinanceCurrentPrice] = useState(1)
   const [binanceFundingRate, setBinanceFundingRate] = useState(null)
   const [binanceNextFundingRate, setBinanceNextFundingRate] = useState(null)
+  const [binanceOrderBookTime, setBinanceOrderBookTime] = useState(null)
+  const [binanceBid1, setBinanceBid1] = useState(
+    Array.from({ length: 2 }, () => null)
+  )
+  const [binanceAsk1, setBinanceAsk1] = useState(
+    Array.from({ length: 2 }, () => null)
+  )
 
   const [dydxMarkPrice, setDydxMarkPrice] = useState(1)
   const [dydxMarkPriceTime, setDydxMarkPriceTime] = useState(null)
   const [dydxIndexPrice, setDydxIndexPrice] = useState(1)
-  const [dydxFundingRate, setDydxFundingRate] = useState(null)
-  const [dydxNextFundingRate, setDydxNextFundingRate] = useState(null)
+  const [dydxFundingRate, setDydxFundingRate] = useState([])
+  const [dydxNextFundingRate, setDydxNextFundingRate] = useState([])
+  const [dydxBid1, setDydxBid1] = useState(
+    Array.from({ length: 2 }, () => null)
+  )
+  const [dydxAsk1, setDydxAsk1] = useState(
+    Array.from({ length: 2 }, () => null)
+  )
 
   const futuresBinance = "btcusdt"
   const futuresDydx = "BTC-USD"
 
   useEffect(() => {
-    const markPriceEvery1sec = new WebSocket(
+    const binanceMarkPriceEvery1sec = new WebSocket(
       `wss://fstream.binance.com/ws/${futuresBinance}@markPrice@1s`
     )
-    markPriceEvery1sec.onmessage = ({ data }) => {
+    binanceMarkPriceEvery1sec.onmessage = ({ data }) => {
       const json = JSON.parse(data)
       setBinanceMarkPrice(json.p)
       setBinanceMarkPriceTime(json.E)
@@ -39,7 +53,17 @@ export default function Binancedydx() {
       setBinanceFundingRate(json.r)
       setBinanceNextFundingRate(json.T)
     }
+    const binanceOrderBookEvery100ms = new WebSocket(
+      `wss://fstream.binance.com/stream?streams=${futuresBinance}@depth10@500ms` // 250ms, 500ms or 100ms
+    )
+    binanceOrderBookEvery100ms.onmessage = ({ data }) => {
+      const json = JSON.parse(data).data
+      setBinanceOrderBookTime(json.E)
+      setBinanceBid1(json.b[0])
+      setBinanceAsk1(json.a[0])
+    }
 
+    // dydx
     setInterval(async () => {
       const { data } = await axios.get("https://api.dydx.exchange/v3/markets", {
         params: {
@@ -52,32 +76,35 @@ export default function Binancedydx() {
       setDydxFundingRate(data.markets[futuresDydx].nextFundingRate)
       setDydxNextFundingRate(data.markets[futuresDydx].nextFundingAt)
     }, 1000)
+    setInterval(async () => {
+      const { data } = await axios.get(
+        "https://api.dydx.exchange/v3/orderbook/BTC-USD"
+      )
+      setDydxAsk1(data.asks[0])
+      setDydxBid1(data.bids[0])
+    }, 1000)
   }, [])
 
   return (
     <>
       <Header path="/binancedydx" />
       <Typography sx={{ ml: 3, mt: 1, mb: 2 }} variant="h5" gutterBottom>
-        · 분석
+        · Analyze
       </Typography>
       <Paper elevation={1} sx={{ m: 3 }}>
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
               <TableRow>
-                <TableCell>Exchange</TableCell>
-                <TableCell>Symbol</TableCell>
-                <TableCell align="right">Mark Price Time</TableCell>
-                <TableCell align="right">Mark Price ($)</TableCell>
+                <TableCell width={"5%"}>Exchange</TableCell>
+                <TableCell width={"5%"}>Symbol</TableCell>
+                <TableCell align="right">매수호가/수량 ($/BTC)</TableCell>
+                <TableCell align="right">매도호가/수량 ($/BTC)</TableCell>
                 <TableCell align="right">Index Price ($)</TableCell>
                 <TableCell align="right">
                   Mark Price – Index Price ($)
                 </TableCell>
-                <TableCell align="right">
-                  Mark Price / Index Price (%)
-                </TableCell>
                 <TableCell align="right">Funding Rate</TableCell>
-                <TableCell align="right">Next Funding Rate Time</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -91,13 +118,10 @@ export default function Binancedydx() {
                   {futuresBinance}
                 </TableCell>
                 <TableCell align="right">
-                  {new Date(binanceMarkPriceTime).toLocaleString()}
+                  {thousandsSeparator(binanceBid1[0])} / {binanceBid1[1]}
                 </TableCell>
                 <TableCell align="right">
-                  {thousandsSeparator(binanceMarkPrice)}
-                </TableCell>
-                <TableCell align="right">
-                  {thousandsSeparator(binanceIndexPrice)}
+                  {thousandsSeparator(binanceAsk1[0])} / {binanceAsk1[1]}
                 </TableCell>
                 <TableCell align="right">
                   {binanceMarkPrice - binanceIndexPrice}
@@ -106,9 +130,6 @@ export default function Binancedydx() {
                   {(binanceMarkPrice / binanceIndexPrice - 1) * 100}
                 </TableCell>
                 <TableCell align="right">{binanceFundingRate}</TableCell>
-                <TableCell align="right">
-                  {new Date(binanceNextFundingRate).toLocaleString()}
-                </TableCell>
               </TableRow>
 
               <TableRow
@@ -121,13 +142,10 @@ export default function Binancedydx() {
                   {futuresDydx}
                 </TableCell>
                 <TableCell align="right">
-                  {new Date(dydxMarkPriceTime).toLocaleString()}
+                  {thousandsSeparator(dydxBid1.price)} / {dydxBid1.size}
                 </TableCell>
                 <TableCell align="right">
-                  {thousandsSeparator(dydxMarkPrice)}
-                </TableCell>
-                <TableCell align="right">
-                  {thousandsSeparator(dydxIndexPrice)}
+                  {thousandsSeparator(dydxAsk1.price)} / {dydxAsk1.size}
                 </TableCell>
                 <TableCell align="right">
                   {dydxMarkPrice - dydxIndexPrice}
@@ -136,12 +154,9 @@ export default function Binancedydx() {
                   {(dydxMarkPrice / dydxIndexPrice - 1) * 100}
                 </TableCell>
                 <TableCell align="right">{dydxFundingRate}</TableCell>
-                <TableCell align="right">
-                  {new Date(dydxNextFundingRate).toLocaleString()}
-                </TableCell>
               </TableRow>
 
-              <TableRow
+              {/* <TableRow
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
@@ -168,7 +183,7 @@ export default function Binancedydx() {
                 <TableCell align="right"></TableCell>
                 <TableCell align="right"></TableCell>
                 <TableCell align="right"></TableCell>
-              </TableRow>
+              </TableRow> */}
             </TableBody>
           </Table>
         </TableContainer>
