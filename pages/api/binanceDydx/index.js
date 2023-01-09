2e7adc4ed005d36247b3fc4073b8da0b70db64c7
2018009070 <youngan@hanyang.ac.kr>
2023-01-09 18:18:34 +0900

    get db data by 10 minutes

--- pages/api/binanceDydx/index.js
+++ pages/api/binanceDydx/index.js
@@ -4,7 +4,11 @@ export default async function handler(req, res) {
   const connection = await pool.getConnection(async (conn) => conn)
 
   const [rows] = await connection.query(
-    `SELECT binanceBid1, dydxBid1, DATE_FORMAT(createdTime,'%m/%d %H:%i') createdTime FROM binanceDydxPrice ORDER BY createdTIme ASC;`
+    // 10분마다
+    `select binanceBid1, dydxBid1, DATE_FORMAT(createdTime,'%m/%d %H:%i') createdTime from binanceDydxPrice where DATE_FORMAT(createdTime,'%i') like '%0' order by createdtime asc`
+
+    // 1분마다
+    // `SELECT binanceBid1, dydxBid1, DATE_FORMAT(createdTime,'%m/%d %H:%i') createdTime FROM binanceDydxPrice ORDER BY createdTIme ASC;`
   )
 
   connection.release()
